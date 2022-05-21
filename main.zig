7ec95aa48a2e13a3d2f686f31b3890a4f68a298e
ousttrue <ousttrue@gmail.com>
2022-05-22 02:16:48 +0900

    glfw.Window.create

--- main.zig
+++ main.zig
@@ -5,6 +5,18 @@ const glfw = @import(".gyro/mach-glfw-hexops-github.com-dae779de/pkg/src/main.zi
 pub fn main() anyerror!void {
     try glfw.init(.{});
     defer glfw.terminate();
+
+    const window = try glfw.Window.create(640, 480, "zig glfw sample", null, null, .{
+        .client_api = .no_api,
+    });
+    defer window.destroy();
+
+    while (!window.shouldClose()) {
+
+        
+
+        try glfw.pollEvents();
+    }
 }
 
 test "basic test" {
